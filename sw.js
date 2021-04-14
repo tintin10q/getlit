fc01eb9fb4b9c83cc6f8b9759c02896b847149fd
Quinten <tintin10q@hotmail.com>
2021-04-14 02:04:37 +0200

    Fixed service worker chaching + removing old cache

--- sw.js
+++ sw.js
@@ -13,22 +13,57 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function(cache) {
-                console.log('Opened cache');
+                console.log('Saved files in service worker cache');
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                    // Cache hit - return response
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request);
-                }
-            )
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then( // then on a return that is pretty cool
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 /*|| response.type !== 'basic'*/) {
+              return response;
+            }
+
+            // Clone because you can only consume a stream once
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
     );
+});
+
+// Make sure you only have the v2 cache
+self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = ['getlit-cache-v2'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) { 
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
